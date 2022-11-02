fbbe6af8af1b304c66cc4e0b3c21e16579a12781
JeffCid20 <jeffopuni@gmail.com>
2022-11-02 00:01:50 +0000

    Fixed header file

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,7 +1,10 @@
 #ifndef main_h
 #define main_h
 
-int_putchar(char c);
+#include <stdio.h>
+#include <stdlib.h>
+
+int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
